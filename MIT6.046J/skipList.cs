480b2a21ba075252e98a91e5a9ce758502223f83
Ron A. <37012505+Ron-Ash@users.noreply.github.com>
2026-03-19 09:21:47 +1000

    started work on skipList

--- MIT6.046J/skipList.cs
+++ MIT6.046J/skipList.cs
@@ -15,29 +15,74 @@ public class SkipNode<T> where T : IComparable<T> {
 
 public class SkipList<T> where T : IComparable<T> {
 	private List<SkipNode<T>> Heads {get;}
+	private Random CoinFlip = new Random();
+	private bool Promote => CoinFlip.Next(2)==1;
 	
 	public SkipList() {
 		Heads = new List<SkipNode<T>>();
 	}
 	
-	public T? Search(T item) {
+	private List<SkipNode<T>> PathwayDescent(T item) {
+		List<SkipNode<T>> pathway = new List<SkipNode<T>>();
 		var node = Heads[Heads.Count-1];
+		pathway.Add(node);
 		for (int level=Heads.Count-1; level>=0; level--) {
-			while (node.Next != null && Comparer<T>.Default.Compare(node.Next.Value, item)<0) {
+			while (node.Next != null && Comparer<T>.Default.Compare(node.Next.Value, item)<=0) {
 				node = node.Next;
 			}
+			pathway.Add(node);
 			if (level > 0) node = node.Down;
-			else return Comparer<T>.Default.Compare(node.Value, item)==0 ? node.Value: null;
+			else break;
 		}
-		return null;
+		return pathway;
 	}
 	
-	public void Insert(T item) {
+	public T? Search(T item) {
+		List<SkipNode<T>> pathway = PathwayDescent(item);
+		var node = pathway[pathway.Count-1];
+		return Comparer<T>.Default.Compare(node.Value, item)==0 ? node.Value : default(T);
+	}
+	
+	public void Insert(T item) {	
+		if (Heads.Count == 0) {
+			Heads.Add(new SkipNode<T>(item));
+			return;
+		}
+
+		List<SkipNode<T>> pathway = PathwayDescent(item);
+		var node = pathway[pathway.Count-1];
 		
+		var child = new SkipNode<T>(item);
+		child.Next = node.Next;
+		child.Prev = node;
+		node.Next = child;
+		
+		int i = pathway.Count-2;
+		while (Promote) {
+			var tmp = new SkipNode<T>(item);
+			if (i<0) {
+				tmp.Next = pathway[i].Next;
+				tmp.Prev = node;
+				pathway[i].Next = tmp;
+				tmp.Down = child;
+			} else {
+				Heads.Add(tmp);
+			}
+			child = tmp;
+			i--;
+		}
 	}
 	
 	public void Delete(T item) {
-		
+		List<SkipNode<T>> pathway = PathwayDescent(item);
+		var node = pathway[pathway.Count-1];
+		if (Comparer<T>.Default.Compare(node.Value, item)!=0) return;
+		for (int level=pathway.Count-1; level>=0; level--) {
+			if (Comparer<T>.Default.Compare(pathway[level].Value, item)!=0) return;
+			node = pathway[level];
+			node.Prev.Next = node.Next;
+			node.Next.Prev = node.Prev;
+		}
 	}
 }
 
